1ebbd0c56589af58c0b11e4f1645b591d7e6a0a2
ndavido <ndavido02@gmail.com>
2024-01-20 18:47:20 +0000

    :sparkles: Initial OCR

--- __init__.py
+++ __init__.py
@@ -1,1 +1,14 @@
 #! usr/bin/env python3
+import pytesseract
+from PIL import Image
+from PIL import ImageEnhance
+image = Image.open("Reciepts/Screenshot_20240117_130009_Snapchat.jpg")
+
+# Preprocessing the image
+enhancer = ImageEnhance.Contrast(image)
+enhanced_image = enhancer.enhance(2)  # Increase contrast
+enhanced_image = enhanced_image.convert("L")  # Convert to grayscale
+
+text = pytesseract.image_to_string(enhanced_image)
+
+print(text)
